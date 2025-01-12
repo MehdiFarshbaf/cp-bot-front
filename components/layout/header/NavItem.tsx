25fc03a555c636b42a4ad769f9503257c8f76083
Mehdi Farshbaf <mehdifarshbaf92@gmail.com>
2025-01-12 09:22:35 +0330

    active path

--- components/layout/header/NavItem.tsx
+++ components/layout/header/NavItem.tsx
@@ -17,7 +17,7 @@ const NavItem=({menu}:{menu:IMenu})=>{
     },[pathname])
     return(
         <Link
-            className={`text-base font-normal text-gray1 hover:text-brand transition-all duration-300 ${ isCurrent ? "text-brand":"" }`}
+            className={`text-base font-normal text-gray1 hover:text-brand transition-all duration-300 ${ menu.path == pathname ? "!text-brand ":"" }`}
             key={menu.id} href={menu.path}>{menu.label}</Link>
     )
 }
